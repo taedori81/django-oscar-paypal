64125e2822bc4bbf19fdee58b8d9ca2464431ebc
taedori <taedori@outlook.com>
2015-11-23 14:14:01 -0800

    Change RedirectView

--- paypal/express/views.py
+++ paypal/express/views.py
@@ -61,7 +61,8 @@ class RedirectView(CheckoutSessionMixin, RedirectView):
 
     def get_redirect_url(self, **kwargs):
         try:
-            basket = self.request.basket
+            # Change to use DeferredTaxMixin
+            basket = self.build_submission()['basket']
             url = self._get_redirect_url(basket, **kwargs)
         except PayPalError as ppe:
             messages.error(
